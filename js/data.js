1680cb3109da1620b9fbf740c1c1136e122ce4a6
wl02008923 <103306024@nccu.edu.tw>
2020-05-17 21:01:28 +0800

    init

---
+++ js/data.js
@@ -0,0 +1,152 @@
+const jp = [{
+  "month": "December",
+  "number": 631547
+}, {
+  "month": "January",
+  "number": 564957
+}, {
+  "month": "February",
+  "number": 446706
+}, {
+  "month": "March",
+  "number": 33074
+}];
+
+const ch = [{
+  "month": "December",
+  "number": 342724
+}, {
+  "month": "January",
+  "number": 360551
+}, {
+  "month": "February",
+  "number": 35995
+}, {
+  "month": "March",
+  "number": 7797
+}];
+
+const hk = [{
+  "month": "December",
+  "number": 340485
+}, {
+  "month": "January",
+  "number": 323327
+}, {
+  "month": "February",
+  "number": 51728
+}, {
+  "month": "March",
+  "number": 4961
+}];
+
+const kr = [{
+  "month": "December",
+  "number": 246990
+}, {
+  "month": "January",
+  "number": 245645
+}, {
+  "month": "February",
+  "number": 99121
+}, {
+  "month": "March",
+  "number": 2064
+}];
+
+const vn = [{
+  "month": "December",
+  "number": 136851
+}, {
+  "month": "January",
+  "number": 145323
+}, {
+  "month": "February",
+  "number": 140331
+}, {
+  "month": "March",
+  "number": 48638
+}];
+
+const us = [{
+  "month": "December",
+  "number": 150266
+}, {
+  "month": "January",
+  "number": 141438
+}, {
+  "month": "February",
+  "number": 81012
+}, {
+  "month": "March",
+  "number": 58017
+}];
+
+const tl = [{
+  "month": "December",
+  "number": 134369
+}, {
+  "month": "January",
+  "number": 119937
+}, {
+  "month": "February",
+  "number": 98307
+}, {
+  "month": "March",
+  "number": 26826
+}];
+
+const pl = [{
+  "month": "December",
+  "number": 105791
+}, {
+  "month": "January",
+  "number": 115628
+}, {
+  "month": "February",
+  "number": 63269
+}, {
+  "month": "March",
+  "number": 28319
+}];
+
+const sg = [{
+  "month": "December",
+  "number": 99865
+}, {
+  "month": "January",
+  "number": 77845
+}, {
+  "month": "February",
+  "number": 52618
+}, {
+  "month": "March",
+  "number": 17383
+}];
+
+const ml = [{
+  "month": "December",
+  "number": 78128
+}, {
+  "month": "January",
+  "number": 53065
+}, {
+  "month": "February",
+  "number": 50234
+}, {
+  "month": "March",
+  "number": 15430
+}];
+
+const data = {
+  Japan: jp,
+  China: ch,
+  Hongkung: hk,
+  Korea: kr,
+  Vietnam: vn,
+  Usa: us,
+  Thailand: tl,
+  Philippines: pl,
+  Singapore: sg,
+  Malaysia: ml
+}
