cdefb6c63a547b7a49799fd7cab723d13e605f30
wl02008923 <103306024@nccu.edu.tw>
2020-05-21 22:31:43 +0800

    add animation

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ function draw(dataset) {
     .range([margin.left, width - margin.right])
     .padding(0.1);
 
-  let y2Scale = d3.scaleLinear()
+  let yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, d => d.number)])
     .range([height - margin.bottom, margin.top]);
 
@@ -44,18 +44,11 @@ function draw(dataset) {
     .call(d3.axisBottom(xScale)
       .tickPadding(padding));
   // y axis left
-  // axis.append('g')
-  //   .attr("transform", `translate(${margin.left}, 0)`)
-  //   .call(d3.axisLeft(y1Scale)
-  //     .tickFormat(d => d / 10000 + " 萬")
-  //     .tickPadding(padding))
-  //   .selectAll('text')
-  //   .style('fill', orange)
-  //   .style('font-weight', 'bold');
+
   // y axis right
   axis.append('g')
     .attr('transform', `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y2Scale)
+    .call(d3.axisLeft(yScale)
       .tickPadding(padding))
     .selectAll('text')
     .style('fill', green)
@@ -66,13 +59,7 @@ function draw(dataset) {
     .attr('x', width / 2)
     .attr('y', height - 5)
     .text('月份');
-  // y axis left title
-  // axis.append('text')
-  //   .attr('text-anchor', 'end')
-  //   .attr('x', margin.left - padding)
-  //   .attr('y', margin.top - 20)
-  //   .text('旅客人數');
-  // y axis right title
+
   axis.append('text')
     .attr('text-anchor', 'end')
     .attr('x', margin.left - padding)
@@ -85,183 +72,63 @@ function draw(dataset) {
   let data = dataset.slice(0, 4);
   // let barChart = chart.append('g').attr('class', 'bar-chart');
   let lineChart = chart.append('g').attr('class', 'line-chart');
-  lineChart.append('path')
+  let line_tooltip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-30, 0])
+    .html(d => {
+      return `<div>入境人數 (${d.month})</div><div>${formatComma(d.number)} 人</div>`;
+    });
+  lineChart.call(line_tooltip);
+
+  let line = lineChart.append('path')
     .datum(data)
     .attr('class', 'line')
     .attr('fill', 'none')
     .attr('stroke', green)
-    .attr('stroke-width', 1.5)
+    .attr('stroke-width', 2)
     .attr('d', d3.line()
       .x(d => xScale(d.month) + xScale.bandwidth() / 2)
-      .y(d => y2Scale(d.number)));
-  update(data);
+      .y(d => yScale(d.number)));
 
-  // slider
-  // let minYear = d3.min(dataset, d => d.year);
-  // let maxYear = d3.max(dataset, d => d.year);
-  // let slider = d3.sliderHorizontal()
-  //   .domain([minYear, maxYear])
-  //   .step(1)
-  //   .width(width - margin.left - margin.right)
-  //   .tickFormat(d => d + " 年")
-  //   .displayValue(true)
-  //   .on('onchange', val => {
-  //     let currentData = dataset.slice((val - minYear) * 12, (val - minYear + 1) * 12);
-  //     update(currentData);
-  //   });
-  //
-  // d3.select('#slider')
-  //   .append('svg')
-  //   .attr('width', width)
-  //   .attr('height', 120)
-  //   .append('g')
-  //   .attr('transform', `translate(${margin.left}, 80)`)
-  //   .call(slider)
-  //   .selectAll('text')
-  //   .style('font-size', 14);
-  // d3.select('.parameter-value')
-  //   .append('image')
-  //   .attr('xlink:href', 'hsr.svg')
-  //   .attr('width', 80)
-  //   .attr('x', -40)
-  //   .attr('y', -80);
+  let totalLength = line.node().getTotalLength();
+  line.attr('stroke-dasharray', totalLength + " " + totalLength)
+    .attr('stroke-dashoffset', totalLength)
+    .transition()
+    .duration(2000)
+    .ease(d3.easeLinear)
+    .attr('stroke-dashoffset', 0);
 
-  function update(newData) {
-    // let sumPassenger = d3.sum(newData, d => d.passenger);
-    // let minPassenger = d3.min(newData, d => d.passenger);
-    // let maxPassenger = d3.max(newData, d => d.passenger);
-    // let avgPassenger = sumPassenger / newData.length;
-    //
-    // let sumNumber = d3.sum(newData, d => d.number);
-    // let minNumber = d3.min(newData, d => d.number);
-    // let maxNumber = d3.max(newData, d => d.number);
-    // let avgNumber = sumNumber / newData.length;
-    //
-    // document.querySelector("#year").innerHTML = newData[0].year;
-    // document.querySelector("#sumPassenger").innerHTML = formatComma(sumPassenger);
-    // document.querySelector("#avgPassenger").innerHTML = formatComma(formatRound(avgPassenger));
-    // document.querySelector("#sumNumber").innerHTML = formatComma(sumNumber);
-    // document.querySelector("#avgNumber").innerHTML = formatComma(formatRound(avgNumber));
+  let dot = lineChart.selectAll('.dot').data(data);
+  dot.enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('cx', d => xScale(d.month) + xScale.bandwidth() / 2)
+    .attr('cy', d => yScale(d.number))
+    .attr('fill', d => "white")
+    .attr('stroke', green)
+    .attr('stroke-width', 2)
+    .attr('r', 0)
+    .on("mouseover", line_tooltip.show)
+    .on("mouseout", line_tooltip.hide)
+    .transition()
+    .duration(500)
+    .delay((d, i) => i * 500)
+    .ease(d3.easeLinear)
+    .attr('r', 5);
 
-    // let sortPassenger = d => {
-    //   if (d.passenger == minPassenger) {
-    //     document.querySelector("#minPassenger").innerHTML = `${formatComma(minPassenger)} (${d.month}月)`;
-    //     return 0.2;
-    //   } else if (d.passenger == maxPassenger) {
-    //     document.querySelector("#maxPassenger").innerHTML = `${formatComma(maxPassenger)} (${d.month}月)`;
-    //     return 1;
-    //   } else {
-    //     return 0.6;
-    //   }
-    // };
-
-    // let sortNumber = d => {
-    //   if (d.number == minNumber) {
-    //     document.querySelector("#minNumber").innerHTML = `${formatComma(minNumber)} (${d.month}月)`;
-    //     return green;
-    //   } else if (d.number == maxNumber) {
-    //     document.querySelector("#maxNumber").innerHTML = `${formatComma(maxNumber)} (${d.month}月)`;
-    //     return green;
-    //   } else {
-    //     return white;
-    //   }
-    // };
-
-    // let bar_tooltip = d3.tip()
-    //   .attr('class', 'd3-tip')
-    //   .offset([-10, 0])
-    //   .html(d => {
-    //     return `<div>旅客人數 (${d.month}月)</div><div>${formatComma(d.passenger)} 人</div>`;
-    //   });
-    // barChart.call(bar_tooltip);
-
-    // let bar = barChart.selectAll('.bar').data(newData);
-    // bar.exit().remove();
-    // bar.enter()
-    //   .append('rect')
-    //   .attr('class', 'bar')
-    //   .attr('fill', orange)
-    //   .attr('opacity', d => sortPassenger(d))
-    //   .attr('x', d => xScale(d.month))
-    //   .attr('y', d => y1Scale(d.passenger))
-    //   .attr('width', xScale.bandwidth())
-    //   .attr('height', d => y1Scale(0) - y1Scale(d.passenger))
-    //   .on("mouseover", bar_tooltip.show)
-    //   .on("mouseout", bar_tooltip.hide);
-    // bar.transition()
-    //   .duration(500)
-    //   .ease(d3.easeLinear)
-    //   .attr('opacity', d => sortPassenger(d))
-    //   .attr('y', d => y1Scale(d.passenger))
-    //   .attr('height', d => y1Scale(0) - y1Scale(d.passenger));
-    //
-    // let barLabel = barChart.selectAll('.bar-label').data(newData);
-    // barLabel.exit().remove();
-    // barLabel.enter()
-    //   .append('text')
-    //   .attr('class', 'bar-label')
-    //   .text(d => formatRound(d.passenger / 10000) + " 萬")
-    //   .attr('x', d => xScale(d.month) + xScale.bandwidth() / 2)
-    //   .attr('y', d => y1Scale(d.passenger) + 25)
-    //   .attr('text-anchor', 'middle')
-    //   .attr('font-size', 12)
-    //   .attr('fill', white);
-    // barLabel.transition()
-    //   .duration(500)
-    //   .ease(d3.easeLinear)
-    //   .text(d => formatRound(d.passenger / 10000) + " 萬")
-    //   .attr('y', d => y1Scale(d.passenger) + 25);
-
-    let line_tooltip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-30, 0])
-      .html(d => {
-        return `<div>入境人數 (${d.month})</div><div>${formatComma(d.number)} 人</div>`;
-      });
-    lineChart.call(line_tooltip);
-
-    let line = lineChart.selectAll('.line').datum(newData);
-    line.transition()
-      .duration(500)
-      .ease(d3.easeLinear)
-      .attr('d', d3.line()
-        .x(d => xScale(d.month) + xScale.bandwidth() / 2)
-        .y(d => y2Scale(d.number)));
-
-    let dot = lineChart.selectAll('.dot').data(newData);
-    dot.exit().remove();
-    dot.enter()
-      .append('circle')
-      .attr('class', 'dot')
-      .attr('cx', d => xScale(d.month) + xScale.bandwidth() / 2)
-      .attr('cy', d => y2Scale(d.number))
-      .attr('fill', d => "white")
-      .attr('stroke', green)
-      .attr('stroke-width', 2)
-      .attr('r', 6)
-      .on("mouseover", line_tooltip.show)
-      .on("mouseout", line_tooltip.hide);;
-    dot.transition()
-      .duration(500)
-      .ease(d3.easeLinear)
-      .attr('cx', d => xScale(d.month) + xScale.bandwidth() / 2)
-      .attr('cy', d => y2Scale(d.number))
-      .attr('fill', d => sortNumber(d));
-
-    let dotLabel = lineChart.selectAll('.dot-label').data(newData);
-    dotLabel.exit().remove();
-    dotLabel.enter()
-      .append('text')
-      .attr('class', 'dot-label')
-      .text(d => formatComma(d.number))
-      .attr('x', d => xScale(d.month) + xScale.bandwidth() / 2)
-      .attr('y', d => y2Scale(d.number) - 15)
-      .attr('text-anchor', 'middle')
-      .attr('font-size', 12);
-    dotLabel.transition()
-      .duration(500)
-      .ease(d3.easeLinear)
-      .text(d => formatComma(d.number))
-      .attr('y', d => y2Scale(d.number) - 15);
-  }
+  let dotLabel = lineChart.selectAll('.dot-label').data(data);
+  dotLabel.enter()
+    .append('text')
+    .attr('class', 'dot-label')
+    .text(d => formatComma(d.number))
+    .attr('x', d => xScale(d.month) + xScale.bandwidth() / 2)
+    .attr('y', d => yScale(d.number) - 15)
+    .attr('text-anchor', 'middle')
+    .attr('font-size', 12)
+    .attr('opacity', 0)
+    .transition()
+    .duration(500)
+    .delay((d, i) => i * 500)
+    .ease(d3.easeLinear)
+    .attr('opacity', 1);
 }
